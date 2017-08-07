c7d112323966ff9a5838512eed41af0c798e9432
roman.voievidko <voievidko@gmail.com>
2017-08-08 02:50:47 +0300

    Add java docs

--- src/main/java/task1/Anagrams.java
+++ src/main/java/task1/Anagrams.java
@@ -4,10 +4,25 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 public class Anagrams {
+
+    /**
+     * Method define if two words are anagrams.
+     *
+     * Method checks for nulls, lengths and empty Strings, if they exists
+     * @throws IllegalArgumentException
+     *
+     * @param word1 first word
+     * @param word2 second word
+     * @return String message "Strings are anagrams" if words are anagrams
+     * or letters you need to change in first word to
+     */
     public String isAnagram(String word1, String word2) {
+        if (word1 == null || word2 == null) throw new IllegalArgumentException("Words can't be null");
+        if (word1 == "" || word2 == "") throw new IllegalArgumentException("One or two words is empty");
         if (word1.length() != word2.length()) throw new IllegalArgumentException("Words have different length");
 
-        //TODO Check if word consist only from letters
+        wordValidator(word1);
+        wordValidator(word2);
 
         char [] word1array = word1.toLowerCase().toCharArray();
         char [] word2array = word2.toLowerCase().toCharArray();
@@ -18,20 +33,38 @@ public class Anagrams {
         if (Arrays.equals(word1array, word2array)) {
             return "Strings are anagrams";
         } else {
-            return find(word1array, word2array);
+            return findUniqueCharacters(word1array, word2array);
         }
     }
 
-    public String find (char [] word1array, char [] word2array){
-        HashSet<Character> unique = new HashSet<>();
-        HashSet<Character> common = new HashSet<>();
+    /**
+     * Method find all the letters you need to change in the first
+     * string to make the two strings anagrams.
+     * Complexity of this method is O(1)
+     * @param word1array first char array
+     * @param word2array second char array
+     * @return String of characters
+     */
+    private String findUniqueCharacters(char[] word1array, char[] word2array){
+        HashSet<Character> word1Set = new HashSet<>();
+        HashSet<Character> word2Set = new HashSet<>();
 
         for(int i = 0; i < word1array.length; i++){
-            unique.add(word1array[i]);
-            common.add(word2array[i]);
+            word1Set.add(word1array[i]);
+            word2Set.add(word2array[i]);
         }
-        unique.removeAll(common);
+        word1Set.removeAll(word2Set);
 
-        return unique.toString();
+        return word1Set.toString();
+    }
+
+    /**
+     * Method validates word. Word can contain only letters.
+     * Complexity of this method is O(n)
+     * @throws IllegalArgumentException if word contain other symbols
+     * @param word for validation
+     */
+    private void wordValidator (String word){
+        if (!word.matches("[a-zA-Z]+")) throw new IllegalArgumentException("Words can contain only letters");
     }
 }
--- src/test/java/task1/AnagramsTest.java
+++ src/test/java/task1/AnagramsTest.java
@@ -9,12 +9,19 @@ public class AnagramsTest {
     @Test
     public void twoWordsContainsAnagramTest(){
         Anagrams anagrams = new Anagrams();
-        String word1 = "abcdef";
-        String word2 = "bcdaef";
         String expected = "Strings are anagrams";
 
-        assertEquals(expected, anagrams.isAnagram(word1, word2));
+        assertEquals(expected, anagrams.isAnagram("abcdef", "bcdaef"));
+        assertEquals(expected, anagrams.isAnagram("acer", "reca"));
+    }
 
+    @Test
+    public void twoWordsContainsAnagramAndUpperCaseWordsTest(){
+        Anagrams anagrams = new Anagrams();
+        String expected = "Strings are anagrams";
+
+        assertEquals(expected, anagrams.isAnagram("Asus", "sUsA"));
+        assertEquals(expected, anagrams.isAnagram("Acer", "acer"));
     }
 
     @Test
@@ -32,6 +39,38 @@ public class AnagramsTest {
         Anagrams anagrams = new Anagrams();
         String word1 = "abcdeff";
         String word2 = "aacdfg";
+        anagrams.isAnagram(word1, word2);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void oneEmptyWordsTest(){
+        Anagrams anagrams = new Anagrams();
+        String word1 = "";
+        String word2 = "aacdfg";
+        anagrams.isAnagram(word1, word2);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void oneWordsNullTest(){
+        Anagrams anagrams = new Anagrams();
+        String word1 = null;
+        String word2 = "word";
+        anagrams.isAnagram(word1, word2);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void oneWordsContainNumberTest(){
+        Anagrams anagrams = new Anagrams();
+        String word1 = "wor5";
+        String word2 = "word";
+        anagrams.isAnagram(word1, word2);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void oneWordsContainSymbolTest(){
+        Anagrams anagrams = new Anagrams();
+        String word1 = "word";
+        String word2 = "wo-d";
         anagrams.isAnagram(word1, word2);
     }
 }
